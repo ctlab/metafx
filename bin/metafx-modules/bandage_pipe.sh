6f388a431bca60ebdfeb477a458e04703c86571c
ivartb <ivart07@mail.ru>
2023-06-06 18:44:36 +0300

    Add BandageNG pipeline for Random Forest visualisation

---
+++ bin/metafx-modules/bandage_pipe.sh
@@ -0,0 +1,214 @@
+#!/usr/bin/env bash
+##########################################################################################
+### MetaFX bandage module – classification model and setup for visualisation in Bandage ##
+##########################################################################################
+
+help_message () {
+    echo ""
+    echo "$(metafx -v)"
+    echo "MetaFX bandage module – Machine Learning methods to train classifier and prepare for visualisation in Bandage (https://github.com/ctlab/BandageNG)"
+    echo "Usage: metafx bandage [<Launch options>] [<Input parameters>]"
+    echo ""
+    echo "Launch options:"
+    echo "    -h | --help                       show this help message and exit"
+    echo "    -w | --work-dir      <dirname>    working directory [default: workDir/]"
+    echo ""
+    echo "Input parameters:"
+    echo "    -f | --feature-dir   <dirname>        directory containing folders with contigs for each category, feature_table.tsv and categories_samples.tsv files. Usually, it is workDir from other MetaFX modules (unique, stats, colored, metafast, metaspades) [mandatory]"
+    echo "         --model         <filename>       file with pre-trained classification model, obtained via 'fit' or 'cv' module (\"workDir/rf_model.joblib\" can be used) [optional, if set '-n', '-d', '-e' will be ignored]"
+    echo "    -n | --n-estimators  <int>            number of estimators in classification model [optional]"
+    echo "    -d | --max-depth     <int>            maximum depth of decision tree base estimator [optional]"
+    echo "    -e | --estimator     [RF, ADA, GBDT]  classification model: RF – Random Forest, ADA – AdaBoost, GBDT – Gradient Boosted Decision Trees [optional, default: RF]"
+    echo "         --draw-graph                     if TRUE performs de Bruijn graph construction by SPAdes to GFA format [default: False]"
+    echo "         --gui                            if TRUE opens Bandage GUI and draw images. Does NOT work on servers with command line interface only [default: False]"
+    echo "         --name          <filename>       name of output file with tree model in text format in workDir [optional, default: tree_model]"
+    echo "";}
+
+
+# Paths to pipelines and scripts
+mfx_path=$(which metafx)
+bin_path=${mfx_path%/*}
+SOFT=${bin_path}/metafx-scripts
+PIPES=${bin_path}/metafx-modules
+pwd=`dirname "$0"`
+
+comment () { ${SOFT}/pretty_print.py "$1" "-"; }
+warning () { ${SOFT}/pretty_print.py "$1" "*"; }
+error   () { ${SOFT}/pretty_print.py "$1" "*"; exit 1; }
+
+
+w="workDir"
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    -h|--help)
+    help_message
+    exit 0
+    ;;
+    -f|--feature-dir)
+    featDir="$2"
+    shift
+    shift
+    ;;
+    --model)
+    modelFile="$2"
+    shift
+    shift
+    ;;
+    -n|--n-estimators)
+    nEstimators="$2"
+    shift
+    shift
+    ;;
+    -d|--max-depth)
+    maxDepth="$2"
+    shift
+    shift
+    ;;
+    -e|--estimator)
+    estimator="$2"
+    shift
+    shift
+    ;;
+    --draw-graph)
+    drawGraph=true
+    shift
+    ;;
+    --gui)
+    gui=true
+    shift
+    ;;
+    --name)
+    outputName="$2"
+    shift
+    shift
+    ;;
+    -w|--work-dir)
+    w="$2"
+    shift
+    shift
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+if [[ ${outputName} ]]; then
+    outputName="${w}/${outputName}"
+else
+    outputName="${w}/tree_model"
+fi
+
+
+
+# ==== Step 1 ====
+
+
+comment "Running step 1: preparing classification model"
+mkdir ${w}
+cmd1="python ${SOFT}/build_model_for_bandage.py --res-file ${outputName}.txt "
+
+if [ ! -d ${featDir} ]; then
+    error "Invalid directory with features provided"
+    exit -1
+fi
+
+if [ ! -f ${featDir}/samples_categories.tsv ]; then
+    error "samples_categories.tsv file missing in ${featDir}"
+fi
+
+if [ ! -f ${featDir}/feature_table.tsv ]; then
+    error "feature_table.tsv file missing in ${featDir}"
+fi
+
+cmd1+="--source-dir ${featDir} "
+
+if [[ ${modelFile} ]]; then
+    cmd1+="--model-file ${modelFile} "
+else
+    if [[ ${nEstimators} ]]; then
+        cmd1+="--tree-num ${nEstimators} "
+    fi
+    if [[ ${maxDepth} ]]; then
+        cmd1+="--max-depth ${maxDepth} "
+    fi
+    if [[ ${estimator} ]] ; then
+        case ${estimator} in
+            "RF") cmd1+="--type-of-forest 0" ;;
+            "GBDT") cmd1+="--type-of-forest 1" ;;
+            "ADA") cmd1+="--type-of-forest 2" ;;
+            *) 
+            error "Unknown classification model type! Please, select from [RF, ADA, GBDT]"
+            exit 1
+            ;;
+        esac
+    fi
+fi
+
+
+
+echo "$cmd1"
+$cmd1
+if [[ $? -eq 0 ]]; then
+    comment "Text model file for visualisation saved in ${outputName}.txt"
+    comment "Step 1 finished successfully!"
+else
+    error "Error during step 1!"
+    exit 1
+fi
+
+
+# ===== Step 2 =====
+
+if [[ ${drawGraph} ]]; then
+    comment "Running step 2: constructing de Brujn graph"
+    cmd2="spades.py --only-assembler --sc -o ${w}/spades_graph "
+    
+    counter=1
+    while read line ; do
+        IFS=$'\t' read -ra cat_samples <<< "${line}"
+        cmd2+="--s${counter} ${featDir}/contigs_${cat_samples[0]}/seq-builder-many/sequences/component.seq.fasta "
+        counter=$((counter + 1))
+    done<${featDir}/categories_samples.tsv
+    
+    echo "$cmd2"
+    echo "Log is saved to ${w}/spades_graph.log"
+    $cmd2 > "${w}/spades_graph.log"
+    if [[ $? -eq 0 ]]; then
+        comment "De Bruijn graph saved to: ${w}/spades_graph/assembly_graph_with_scaffolds.gfa"
+        graph="${w}/spades_graph/assembly_graph_with_scaffolds.gfa"
+    else
+        error "Error during step 1!"
+        exit 1
+    fi
+else
+    comment "Skipping step 2 (de Brujn graph construction): will draw only forest model"
+    graph="\"\""
+fi
+
+
+# ==== Step 3 ====
+
+if [[ ${gui} ]]; then
+    comment "Running step 3: Bandage GUI"
+
+    cmd3="BandageNG load ${graph} ${outputName}.txt"
+    echo "${cmd3}"
+    ${cmd3}
+    if [[ $? -eq 0 ]]; then
+        comment "Step 3 finished successfully!"
+    else
+        error "Error during step 3!"
+        exit 1
+    fi
+else
+    comment "To visualise results in Bandage GUI follow instructions from https://github.com/ctlab/metafx/wiki/#metafx-bandage"
+fi
+
+comment "MetaFX bandage module finished successfully!"
+exit 0
---
+++ bin/metafx-scripts/build_model_for_bandage.py
@@ -0,0 +1,286 @@
+#!/usr/bin/env python
+# Utility for creating and visualisation of machine learning classifier in Bandage
+#-*- coding: UTF-8 -*-
+
+import sys, getopt
+import numpy as np
+import pandas as pd
+
+# for classification with cross-validation
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, AdaBoostClassifier
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
+from sklearn.tree import DecisionTreeClassifier
+
+#dump and load classification model
+from joblib import dump, load
+
+
+def buildModelRandomForest(dataFile, rawLabels, nEstimators, maxDepth):
+    data = pd.read_csv(dataFile, header=0, index_col=0, sep = '\t')
+    if set(data.columns) != set(rawLabels.index):
+        data = data.filter(items=rawLabels.index, axis=1)
+        print("Samples from feature table and metadata does not match! Will use only " + str(data.shape[1]) + " common samples")
+    data = data.T
+    labels = np.array([rawLabels.loc[i, 1] for i in data.index])
+
+    if (nEstimators != 0 and maxDepth != 0):
+        model = RandomForestClassifier(n_estimators=nEstimators, max_depth=maxDepth)
+    elif (nEstimators != 0):
+        model = RandomForestClassifier(n_estimators=nEstimators)
+    elif (maxDepth != 0):
+        model = RandomForestClassifier(max_depth=maxDepth)
+    else:
+        model = RandomForestClassifier()
+    
+    model.fit(data, labels)
+    
+    return model
+
+
+def buildModelGradientBoosting(dataFile, rawLabels, nEstimators, maxDepth):
+    data = pd.read_csv(dataFile, header=0, index_col=0, sep = '\t')
+    if set(data.columns) != set(rawLabels.index):
+        data = data.filter(items=rawLabels.index, axis=1)
+        print("Samples from feature table and metadata does not match! Will use only " + str(data.shape[1]) + " common samples")
+    data = data.T
+    labels = np.array([rawLabels.loc[i, 1] for i in data.index])
+    
+    if (nEstimators != 0 and maxDepth != 0):
+        model = GradientBoostingClassifier(n_estimators=nEstimators, max_depth=maxDepth)
+    elif (nEstimators != 0):
+        model = GradientBoostingClassifier(n_estimators=nEstimators)
+    elif (maxDepth != 0):
+        model = GradientBoostingClassifier(max_depth=maxDepth)
+    else:
+        model = GradientBoostingClassifier()
+    
+    model.fit(data, labels)
+
+    return model
+
+
+def buildModelAdaBoost(dataFile, rawLabels, nEstimators, maxDepth):
+    data = pd.read_csv(dataFile, header=0, index_col=0, sep = '\t')
+    if set(data.columns) != set(rawLabels.index):
+        data = data.filter(items=rawLabels.index, axis=1)
+        print("Samples from feature table and metadata does not match! Will use only " + str(data.shape[1]) + " common samples")
+    data = data.T
+    labels = np.array([rawLabels.loc[i, 1] for i in data.index])
+
+    if (nEstimators != 0 and maxDepth != 0):
+        dTree = DecisionTreeClassifier(max_depth = maxDepth)
+        model = AdaBoostClassifier(n_estimators=nEstimators, estimator=dTree)
+    elif (nEstimators != 0):
+        model = AdaBoostClassifier(n_estimators=nEstimators)
+    elif (maxDepth != 0):
+        dTree = DecisionTreeClassifier(max_depth = maxDepth)
+        model = AdaBoostClassifier(estimator=dTree)
+    else:
+        model = AdaBoostClassifier()
+    
+    model.fit(data, labels)
+    
+    return model
+
+
+def printModelBase(model, resFileName, sourceDir):
+    f = open(resFileName, 'w')
+    prefix = 0
+    features = dict()
+    treeClassifierList = model.estimators_
+    feature_names = model.feature_names_in_
+    classes = model.classes_
+    for tc in treeClassifierList:
+        tree = tc.tree_
+        nodeIds = [0]
+        while(len(nodeIds) > 0):
+            nodeId = nodeIds.pop(0)
+            print("N", nodeId + prefix, sep = "\t", end = "\t", file = f)
+            if tree.children_left[nodeId] == tree.children_right[nodeId]:
+                print(file = f)
+                if tree.n_outputs == 1:
+                    value = tree.value[nodeId][0]
+                else:
+                    value = tree.value[nodeId].T[0]
+                class_name = np.argmax(value)
+                print("C", nodeId + prefix, classes[class_name], sep = "\t", file = f)
+                continue
+            childLeftId = tree.children_left[nodeId]
+            nodeIds.append(childLeftId)
+            print(childLeftId + prefix, sep = "\t", end = "\t", file = f)
+
+            childRightId = tree.children_right[nodeId]
+            nodeIds.append(childRightId)
+            print(childRightId + prefix,  sep = "\t", end = "\t", file = f)
+            print(file = f)
+            feature = feature_names[tree.feature[nodeId]]
+            threshold = tree.threshold[nodeId]
+            classF = feature.split("_")[0]
+            print("C", nodeId + prefix, classF, sep = "\t", file = f)
+            print("F", nodeId + prefix, feature, "{:.2f}".format(threshold), sep = "\t", end = "\n", file = f)
+            if feature in features:
+                features[feature].append(nodeId + prefix)
+            else:
+                features[feature] = [nodeId + prefix]
+        prefix+=tree.node_count
+
+    for fClass in classes:
+        file = open(sourceDir + "/contigs_" + fClass + "/kmers_fasta/component.fasta", 'r')
+        line=file.readline()
+        k_mer_feature = {}
+        while line:
+            feature = fClass + "_" + line[1:].split("_")[0]
+            k_mer = file.readline()[:-1]
+            if feature in features:
+                print("S", feature, k_mer, sep = "\t", file = f)
+            line = file.readline()
+    f.close()
+
+
+def printModelGradientBoosting(model, resFileName, sourceDir):
+    f = open(resFileName, 'w')
+    prefix = 0
+    features = dict()
+    treeClassifierList = model.estimators_
+    feature_names = model.feature_names_in_
+    classes = model.classes_
+    for i in treeClassifierList:
+        for tc in i:
+            tree = tc.tree_
+            nodeIds = [0]
+            while(len(nodeIds) > 0):
+                nodeId = nodeIds.pop(0)
+                print("N", nodeId + prefix, sep = "\t", end = "\t", file = f)
+                if tree.children_left[nodeId] == tree.children_right[nodeId]:
+                    print(file = f)
+                    if tree.n_outputs == 1:
+                        value = tree.value[nodeId][0]
+                    else:
+                        value = tree.value[nodeId].T[0]
+                    class_name = np.argmax(value)
+                    print("C", nodeId + prefix, classes[class_name], sep = "\t", file = f)
+                    continue
+                childLeftId = tree.children_left[nodeId]
+                nodeIds.append(childLeftId)
+                print(childLeftId + prefix, sep = "\t", end = "\t", file = f)
+
+                childRightId = tree.children_right[nodeId]
+                nodeIds.append(childRightId)
+                print(childRightId + prefix,  sep = "\t", end = "\t", file = f)
+                print(file = f)
+                feature = feature_names[tree.feature[nodeId]]
+                threshold = tree.threshold[nodeId]
+                classF = feature.split("_")[0]
+                print("C", nodeId + prefix, classF, sep = "\t", file = f)
+                print("F", nodeId + prefix, feature, "{:.2f}".format(threshold), sep = "\t", end = "\n", file = f)
+                if feature in features:
+                    features[feature].append(nodeId + prefix)
+                else:
+                    features[feature] = [nodeId + prefix]
+            prefix+=tree.node_count
+
+    for fClass in classes:
+        file = open(sourceDir + "/contigs_" + fClass + "/kmers_fasta/component.fasta", 'r')
+        line=file.readline()
+        k_mer_feature = {}
+        while line:
+            feature = fClass + "_" + line[1:].split("_")[0]
+            k_mer = file.readline()[:-1]
+            if feature in features:
+                print("S", feature, k_mer, sep = "\t", file = f)
+            line = file.readline()
+    f.close()
+
+
+def buildAndPrintModel(sourceDir, treeNum, maxDepth, typeOfForest, resFile, model = None):
+    rawLabels = pd.read_csv(sourceDir + '/samples_categories.tsv', sep="\t", index_col=0, header = None)
+    rawLabels.index = rawLabels.index.astype(str)
+    
+    if typeOfForest == 0:
+        if model == None:
+            model = buildModelRandomForest(sourceDir + '/feature_table.tsv', rawLabels, treeNum, maxDepth)
+            dump(model, resFile[:-4]+".joblib")
+        printModelBase(model, resFile, sourceDir)
+    elif typeOfForest == 1:
+        if model == None:
+            model = buildModelGradientBoosting(sourceDir + '/feature_table.tsv', rawLabels, treeNum, maxDepth)
+            dump(model, resFile[:-4]+".joblib")
+        printModelGradientBoosting(model, resFile, sourceDir)
+    else:
+        if model == None:
+            model = buildModelAdaBoost(sourceDir + '/feature_table.tsv', rawLabels, treeNum, maxDepth)
+            dump(model, resFile[:-4]+".joblib")
+        printModelBase(model, resFile, sourceDir)
+
+
+if __name__ == "__main__":
+    sourceDir = ''
+    modelFile = ''
+    treeNum = 100
+    maxDepth = 20
+    typeOfForest = 0
+    resFile = ''
+
+    model = None
+    helpString = 'Please add all mandatory parameters --source-dir --res-file and use optional parameters --model-file --tree-num --max-depth --type-of-forest'
+
+    argv = sys.argv[1:]
+    try:
+        opts, args = getopt.getopt(argv,"h", [ "source-dir=", "res-file=", "model-file=", "tree-num=", "max-depth=", "type-of-forest="])
+    except getopt.GetoptError:
+        print(helpString)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == "-h":
+            print(helpString)
+            sys.exit()
+        elif opt == "--source-dir":
+            sourceDir = arg
+            if sourceDir[0] == "'" or sourceDir[0] == '"':
+                sourceDir = sourceDir[1:]
+            if sourceDir[-1] == "'" or sourceDir[-1] == '"':
+                sourceDir = sourceDir[:-1]
+        elif opt == "--res-file":
+            resFile = arg
+            if resFile[0] == "'" or resFile[0] == '"':
+                resFile = resFile[1:]
+            if resFile[-1] == "'" or resFile[-1] == '"':
+                resFile = resFile[:-1]
+        elif opt == "--model-file":
+            modelFile = arg
+            if modelFile[0] == "'" or modelFile[0] == '"':
+                modelFile = modelFile[1:]
+            if modelFile[-1] == "'" or modelFile[-1] == '"':
+                modelFile = modelFile[:-1]  
+        elif opt == "--tree-num":
+            treeNum = int(arg)
+        elif opt == "--max-depth":
+            maxDepth = int(arg)
+        elif opt == "--type-of-forest":
+            typeOfForest = int(arg)
+            if typeOfForest < 0 or typeOfForest > 2:
+                print("Please use typeOfForest 0 (RandomForest), 1 (GradientBoosting) or 2 (AdaBoost)")
+                sys.exit(2)  
+                
+    print('Source dir:', sourceDir)
+    print('Result file:', resFile)
+    if (modelFile == ''):
+        print('Type of forest:', typeOfForest)
+        print('Number of trees:', treeNum)
+        print('Maximum depth of the tree:', maxDepth)
+    else:
+        print('Model file:', modelFile)
+
+    if (modelFile != ''):
+        model = load(modelFile)
+        if model.__class__.__name__ == 'RandomForestClassifier':
+            typeOfForest = 0
+        elif model.__class__.__name__ == 'GradientBoostingClassifier':
+            typeOfForest = 1
+        elif model.__class__.__name__ == 'AdaBoostClassifier':
+            typeOfForest = 2
+        else:
+            print("Class of model", model.__class__.__name__, "is incorrect. Supported classes: RandomForestClassifier, GradientBoostingClassifier, AdaBoostClassifier")
+            sys.exit(2)
+    buildAndPrintModel(sourceDir, treeNum, maxDepth, typeOfForest, resFile, model)
